ec77f2dd5542a45257f4647d1f3225cf2cde7aa0
Kenneth Schnall <kenneth.schnall@gmail.com>
2017-08-29 18:48:45 -0400

    Finish level 1 problem 2

---
+++ problems/1.2.py
@@ -0,0 +1,18 @@
+# 1.1 Palindrome Tester
+
+def is_palindrome(input_string):
+    if not input_string:
+        return True
+    
+    a_list = []
+    reversed_string = ''
+
+    for c in input_string:
+        a_list.append(c)
+
+    while a_list:
+        reversed_string += str(a_list.pop())
+    
+    result = input_string == reversed_string
+
+    return result
