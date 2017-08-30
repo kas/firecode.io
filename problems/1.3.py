e89c111d6be1660bae1b853bba35538c25dd1ca2
Kenneth Schnall <kenneth.schnall@gmail.com>
2017-08-30 11:39:39 -0400

    Finish level 1 problem 3

---
+++ problems/1.3.py
@@ -0,0 +1,26 @@
+# 1.3 Horizontal Flip
+
+def flip_horizontal_axis(matrix):
+    height = len(matrix)
+
+    if height <= 1:
+        return
+
+    even = False
+
+    if height % 2 == 0:
+        even = True
+
+    upper_bound = height / 2 
+
+    for r in range(0, upper_bound):
+        target_r = r + upper_bound
+
+        if not even:
+            target_r += 1
+
+        temp = matrix[target_r]
+
+        matrix[target_r] = matrix[r]
+
+        matrix[r] = temp
