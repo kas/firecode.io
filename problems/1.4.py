e27bfa6ae2eb1514d1b9cef01f0b463c730ac671
Kenneth Schnall <kenneth.schnall@gmail.com>
2017-09-09 10:41:14 -0400

    Remove whitespace and print statement from level 1 problem 4

--- problems/1.4.py
+++ problems/1.4.py
@@ -13,19 +13,18 @@ def get_binary(number):
 def dec_to_bin(number):
     binary_representation = ''
     remainders_stack = []
-    
+
     while number > 1:
         remainders_stack.append(get_binary(number))
 
         number /= 2
-        
+
         number = int(number)
-    
+
     remainders_stack.append(get_binary(number))
-    
+
     while remainders_stack:
         binary_representation += str(remainders_stack.pop())
-    
+
     return binary_representation
 
-print(dec_to_bin(5))
