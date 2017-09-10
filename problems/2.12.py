963a848f10c45b3e36a4f96923a2c3e65131d44b
Kenneth Schnall <kenneth.schnall@gmail.com>
2017-09-10 19:28:06 -0400

    Finish level 2 problem 12

---
+++ problems/2.12.py
@@ -0,0 +1,13 @@
+# 2.12 Unique Chars in a String
+
+def unique_chars_in_string(input_string):
+    dictionary = {}
+
+    for character in input_string:
+        if character in dictionary:
+            return False
+        else:
+            dictionary[character] = 1
+
+    return True
+
