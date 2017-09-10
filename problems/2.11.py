2c23672b24abf260c23d2e77164454e96d4cf05f
Kenneth Schnall <kenneth.schnall@gmail.com>
2017-09-10 18:38:22 -0400

    Finish level 2 problem 11

---
+++ problems/2.11.py
@@ -0,0 +1,19 @@
+# 2.11 Insert a Node at the Front of a Linked List
+
+class SinglyLinkedList:
+    # constructor
+    def __init__(self):
+        self.head = None
+
+    # method for setting the head of the Linked List
+    def setHead(self, head):
+        self.head = head
+
+    # method for inserting a new node at the start of a Linked List
+    def insert_at_front(self, data):
+        node = Node()
+        node.setData(data)
+        node.setNext(self.head)
+
+        self.setHead(node)
+
