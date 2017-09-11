4f67256b288c6271a20874be319773e0d2334f62
Kenneth Schnall <kenneth.schnall@gmail.com>
2017-09-10 20:18:10 -0400

    Finish level 2 problem 13

---
+++ problems/2.13.py
@@ -0,0 +1,23 @@
+# 2.13 Max Gain
+
+def max_gain(input_list):
+    if not input_list:
+        return 0
+
+    maximum_gain = 0
+    minimum = input_list[0]
+
+    for i in range(0, len(input_list)):
+        if i == 0:
+            continue
+
+        current_gain = input_list[i] - minimum
+
+        if current_gain > maximum_gain:
+            maximum_gain = current_gain
+
+        if input_list[i] < minimum:
+            minimum = input_list[i]
+
+    return maximum_gain
+
