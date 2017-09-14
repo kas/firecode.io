17afeee429a39fc961583ced40e30632fb2da343
Kenneth Schnall <kenneth.schnall@gmail.com>
2017-09-13 23:14:59 -0400

    Improve level 2 problem 14

---
+++ problems/2.14-improved.py
@@ -0,0 +1,17 @@
+# 2.14 Numbers and Ranges ...
+
+def find_range(input_list, input_number):
+    begin = None
+    end = None
+
+    for i in range(0, len(input_list)):
+        if input_list[i] == input_number:
+            if not begin:
+                begin = i
+
+            end = i
+        elif input_list[i] > input_number:
+            break
+
+    return Range(begin, end)
+
