8c028afc99dad5816a2b3bae75ab9d626ae09983
Kenneth Schnall <kenneth.schnall@gmail.com>
2017-09-14 23:08:02 -0400

    Attempt level 2 problem 15

---
+++ problems/2.15.py
@@ -0,0 +1,14 @@
+# 2.15 Power of 4
+
+def is_power_of_four(number):
+    if number and not (number & (number - 1)):
+        count = 0
+
+        while number > 1:
+            number >>= 1
+            count += 1
+
+        return (count % 2) == 0
+
+    return False
+
