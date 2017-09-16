becce35abb2f82a850f95de9225f11352b41baa4
Kenneth Schnall <kenneth.schnall@gmail.com>
2017-09-16 10:12:09 -0400

    Finish level 2 problem 16

---
+++ problems/2.16.py
@@ -0,0 +1,20 @@
+# 2.16 Better Fibonacci
+
+def better_fibonacci(n):
+    if n < 2:
+        return n
+
+    i = 1
+    second_to_last = 0
+    last = 1
+    current = None
+
+    while i < n:
+        current = second_to_last + last
+        second_to_last = last
+        last = current
+
+        i += 1
+
+    return current
+
